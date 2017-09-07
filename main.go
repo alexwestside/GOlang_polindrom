92cdc0efee54b1797c5d080ee7d455250cac8b26
Olexandr Rizhiy <alex@Olexandrs-Mac-mini.local>
2017-09-07 19:00:32 +0300

    _

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"fmt"
 	"strconv"
+	"time"
 )
 
 func Reverse(s string) string {
@@ -15,27 +16,30 @@ func Reverse(s string) string {
 	return reverse
 }
 
-func check_polindrom(Num1 int, Num2 int) {
+func check_polindrom(Num1 int, Num2 int, start time.Time) {
 	res := Num1 * Num2
 	res1 := strconv.Itoa(res)
 	res2 := Reverse(res1)
 	if res1 == res2 {
 		fmt.Println(res)
+		end := time.Now()
+		elapsed := end.Sub(start)
+		fmt.Println(elapsed)
 		os.Exit(0)
 	}
 }
 
-func find_next_prime(num float64, pr int) {
-	for i := 0; i < pr; i++ {
-		_num := num - float64(i)
-		if chekPrime(_num, int(pr)) {
-			check_polindrom(int(num), int(_num))
+func find_next_prime(num int, start time.Time) {
+	for i := 0; i < num; i++ {
+		_num := num - i
+		if chekPrime(_num) {
+			check_polindrom(int(num), int(_num), start)
 		}
 	}
 }
 
-func chekPrime(num float64, pr int) bool{
-	for i := 2; i < int(math.Sqrt(num))+1; i++ {
+func chekPrime(num int) bool {
+	for i := 2; i < int(math.Sqrt(float64(num)))+1; i++ {
 		if int(num)%i == 0 {
 			return false
 		}
@@ -44,14 +48,15 @@ func chekPrime(num float64, pr int) bool{
 }
 
 func main() {
+	start := time.Now()
 	stdin := os.Args[1:]
 	res, _ := strconv.Atoi(stdin[0])
 	num := math.Pow10(res) - 1
 	pr := math.Pow10(res - 1)
 	for i := 0; i < int(pr); i++ {
-		_num := num-float64(i)
-		if chekPrime(_num, int(pr)){
-			find_next_prime(_num, int(pr))
+		_num := int(num) - i
+		if chekPrime(_num) {
+			find_next_prime(_num, start)
 		}
 	}
 }
